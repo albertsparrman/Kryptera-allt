37c6d16abb25cc17fc01b93f7b349f86d0a039a9
albertsparrman <77719327+albertsparrman@users.noreply.github.com>
2022-10-13 13:17:11 +0200

    Jag peakade

--- src/KryptoView.java
+++ src/KryptoView.java
@@ -1,13 +1,42 @@
 import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
-public class KryptoView extends JFrame {
-    private JTextField textField1;
-    private JTextArea textArea1;
 
-    KryptoView() {
-        JPanel calcPanel = new JPanel();
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(600, 200);
-        this.add(calcPanel);
+public class KryptoView{
+    private JTextField fortniteTextField;
+    private JTextArea TextArea;
+    private JPanel Beda;
+
+
+
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+
+        //create the font
+
+        Font customFont = null;
+        try {
+            //create the font to use. Specify the size!
+            customFont = Font.createFont(Font.TRUETYPE_FONT, new File("fortnite.otf")).deriveFont(12f);
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            //register the font
+            ge.registerFont(customFont);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (FontFormatException e) {
+            e.printStackTrace();
+        }
+
+//use the font
+        frame.setFont(customFont);
+
+        frame.setContentPane(new KryptoView().Beda);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
     }
 }
--- src/MVCKrypto.java
+++ src/MVCKrypto.java
@@ -1,6 +1,3 @@
 public class MVCKrypto {
-    public static void main(String[] args) {
-        KryptoView theView = new KryptoView();
-        theView.setVisible(true);
-    }
+
 }
